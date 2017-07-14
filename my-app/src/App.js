dfc33ceb442235ed043529f07219b750cdde5c5c
tomdaniels <thomsondaniels@hotmail.com>
2017-07-14 17:00:29 +1000

    Creates components, sets up prop types

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import './app.css';
 
 class App extends Component {
+  propTypes: {
+    rooms: React.PropTypes.string.isRequired,
+    address: React.PropTypes.string.isRequired,
+    inspection: React.PropTypes.string.isRequired
+  }
   render() {
     return (
-        <div className="app-header">
-          <h2>Welcome!</h2>
+      <div className='page-wrap'>
+        <div className='header-wrap'>
+          <h1 className='headline'>Shortlist</h1>
         </div>
+        <div className='shortlist-wrap'>
+          <ul className='list-format'>
+            <li className='attributes'>{this.props.rooms}</li>
+          </ul>
+          </div>
+      </div>
     );
   }
 }
-
 export default App;
