691d8bb4231767574d472d5484f2d91a2b350d03
tomdaniels <thomsondaniels@hotmail.com>
2017-07-15 20:32:31 +1000

    Establishes property card, with image

--- my-app/src/card-b.js
+++ my-app/src/card-b.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import './app.css';
+import App from './app'
 import PropTypes from 'prop-types';
 
-class App extends React.Component {
+class PropertyTwo extends React.Component {
   render() {
     return (
       <div className='container-wrap'>
-      <div className='card-wrap'>
-        <img className='thumbnail' src={this.props.imageUrl}/>
         <div className='prop-specifics'>
           <ul className='list-format'>
-            <li className='status'>{this.props.status}</li>
             <li>{this.props.attributes}</li>
             <a href='https://domain.com.au/2013644419' className='prop-address'>{this.props.address}</a>
             </ul>
@@ -24,7 +22,6 @@ class App extends React.Component {
             <div>
               <div className='dates-left'>Auction</div>
               <span className='dates-right-auc'>{this.props.auction}</span>
-              </div>
             </div>
         </div>
       </div>
@@ -32,13 +29,11 @@ class App extends React.Component {
   }
 };
 
-App.defaultProps = {
-  imageUrl: 'https://rimh2.domain.com.au/TJ_VzRm3ltHqkqonTBdexZhzUXI=/fit-in/800x600/filters:format(jpeg)/2013644419_9_0_170606_094015-w1200-h801',
-  status: 'Auction, contact agent',
-  attributes: '5 beds, 3 baths, 2 parking',
+PropertyTwo.defaultProps = {
+  attributes: 'something different',
   address: '20 Fuller Avenue, Earlwood',
   inspection: 'Thu, 29 Jun - 6:00PM',
-  auction: 'Sat, 1 Jul - 1:00PM',
+  auction: 'Sat, 1 Jul - 1:00PM'
 }
 
-export default App;
+export default PropertyTwo;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import PropertyTwo from './card-b'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
