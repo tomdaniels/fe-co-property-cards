42f156f0d714bb7050be5cfb5e6cef5a4cf218eb
tomdaniels <thomsondaniels@hotmail.com>
2017-07-15 13:16:43 +1000

    Sets default props, removes hardcoded data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,36 +1,39 @@
 import React, { Component } from 'react';
 import './app.css';
-import fixtureGroups from './index.js'
+import Fixtures from './index.js'
+import PropTypes from 'prop-types';
 
-class App extends Component {
-  propTypes: {
-    rooms: React.PropTypes.string.isRequired,
-    address: React.PropTypes.string.isRequired,
-    inspection: React.PropTypes.string.isRequired
-  }
+class App extends React.Component {
   render() {
     return (
       <div className='container-wrap'>
         <div className='prop-specifics'>
           <ul className='list-format'>
-            <li>5 beds, 3 baths, 2 parking</li>
-            <a href='https://domain.com.au/2013644419' className='prop-address'>20 Fuller Avenue, Earlwood</a>
+            <li>{this.props.attributes}</li>
+            <a href='https://domain.com.au/2013644419' className='prop-address'>{this.props.address}</a>
             </ul>
         </div>
         <hr className='section-separate'></hr>
         <div className='prop-dates-wrap'>
             <div>
               <div className='dates-left'>Inspection</div>
-              <span className='dates-right'>Thu, 29 Jun - 6:00PM</span>
+              <span className='dates-right'>{this.props.inspection}</span>
             </div>
             <div>
               <div className='dates-left'>Auction</div>
-              <span className='dates-right-auc'>Sat, 1 Jul - 1:00PM</span>
+              <span className='dates-right-auc'>{this.props.auction}</span>
             </div>
         </div>
       </div>
     );
   }
 };
+
+Component.defaultProps = {
+  attributes: '5 beds, 3 baths, 2 parking',
+  address: '20 Fuller Avenue, Earlwood',
+  inspection: 'Thu, 29 Jun - 6:00PM',
+  auction: 'Sat, 1 Jul - 1:00PM'
+}
 
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,22 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 import registerServiceWorker from './registerServiceWorker';
-import Component from './app';
-
-const fixtureGroups = [
-  {
-    component: Component,
-        props: {
-          rooms: '5 beds, 3 baths, 2 parking',
-          address: '20 Fuller Avenue, Earlwood',
-          inspection: 'Thu, 29 Jun - 6:00PM',
-          auction: '"Sat, 1 Jul - 1:00PM"'
-    },
-  },
-];
-
-export default fixtureGroups;
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
